85108d4f0aab3f112e3d0c3b7e0f014c2e3ce7dc
swarupmandal <swarup12x@gmail.com>
2021-04-12 22:08:06 +0530

    first commit

---
+++ src/main/java/com/ms/faf/FriendandFamilyApplication.java
@@ -0,0 +1,13 @@
+package com.ms.faf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendandFamilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendandFamilyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms/faf/FriendandFamilyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.faf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendandFamilyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
